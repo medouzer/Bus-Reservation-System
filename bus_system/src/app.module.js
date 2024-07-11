417a748fd69467f221a54dfc329bd08ca802a7fd
med ouzer <mohamed.ouz2016@gmail.com>
2024-07-11 14:20:45 +0100

    start on the auth

--- bus_system/src/app.module.js
+++ bus_system/src/app.module.js
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MongooseModule.forRoot('mongodb://localhost/bus_school'), AuthModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
